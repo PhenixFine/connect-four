5a8ecd8438400e9f283955ca2d03a56a32a246b6
Danny <64429863+PhenixFine@users.noreply.github.com>
2021-10-04 19:47:59 -0400

    Stage 3 of 5 for JetBrains Academy - Kotlin - Connect Four project.
    GameBoard.kt - changes
    • companion object - was moved to bottom after reading that it should be placed there. The filter used in value dimensions was changed to filterNot and the exclamation mark was removed from within its lambda. Value matches was renamed to notRange, and its lambda was adjusted to return true if not a match. Value wrongDimension was adjusted for its use.
    • values columnRange and boardMap were added next to the array. ColumnRange is 1 to the provided columns. BoardMap is the columnRange numbers associated with the rows value as a mutable map.
    • print() - adjusted to accept a boolean that checks if the rows and columns line should be printed. It defaults to true. Value range was to the top and renamed columnRange.
    • inertDisc() - receives a char to be placed in an array and a column number to place it into. An appropriate error message is printed if the received column string doesn't convert to a number, is out of range, or the column is full. If there is no errors the board array and boardMap is updated and true is returned, otherwise false.
    • updateBoard() - updates the board array ( subtracting 1 from the given row and column ) with the given char named disc. And updates boardMap using the column as the key and subtracting 1 from the row.

--- src/main/kotlin/GameBoard.kt
+++ src/main/kotlin/GameBoard.kt
@@ -2,18 +2,51 @@ import util.getString
 
 class GameBoard private constructor(private val rows: Int = 6, private val columns: Int = 7) {
     private val board = Array(rows) { Array(columns) { ' ' } }
+    private val columnRange = 1..columns
+    private val boardMap = columnRange.associateWith { rows }.toMutableMap()
+
+    fun print(printColumnRows: Boolean = true) {
+        val (pipe, leftPipe, middlePipe, rightPipe) = listOf("║", "╚", "═╩", "═╝")
+
+        if (printColumnRows) println("$rows X $columns board")
+        println(" " + columnRange.joinToString(" "))
+        board.forEach { println(it.joinToString(pipe, pipe, pipe)) }
+        println(leftPipe + middlePipe.repeat(columns - 1) + rightPipe)
+    }
+
+    fun insertDisc(disc: Char, columnString: String): Boolean {
+        val column = columnString.toIntOrNull()
+        var error =
+            if (column == null) "Incorrect column number" else "The column number is out of range (1 - $columns)"
+
+        if (column != null) {
+            boardMap[column]?.let { row: Int ->
+                if (row != 0) {
+                    updateBoard(disc, row, column)
+                    return true
+                } else error = "Column $column is full"
+            }
+        }
+        println(error)
+        return false
+    }
+
+    private fun updateBoard(disc: Char, row: Int, column: Int) {
+        board[row - 1][column - 1] = disc
+        boardMap[column] = row - 1
+    }
 
     companion object {
         fun getGameBoard(): GameBoard {
             val message = "Set the board dimensions (Rows x Columns)\nPress Enter for default (6 x 7)"
-            val dimensions = getString(message).filter { !it.isWhitespace() }
+            val dimensions = getString(message).filterNot { it.isWhitespace() }
             val regex = Regex("\\d+[xX]\\d+")
             var error = "Invalid input"
 
             if (dimensions.isEmpty()) return GameBoard() else if (dimensions.matches(regex)) {
                 val (num1, num2) = dimensions.split("x", "X").map { it.toInt() }
-                val matches = { num: Int -> (5..9).contains(num) }
-                val wrongDimension = if (!matches(num1)) "rows" else if (!matches(num2)) "columns" else ""
+                val notRange = { num: Int -> !(5..9).contains(num) }
+                val wrongDimension = if (notRange(num1)) "rows" else if (notRange(num2)) "columns" else ""
                 if (wrongDimension.isEmpty()) return GameBoard(num1, num2) else {
                     error = "Board $wrongDimension should be from 5 to 9"
                 }
@@ -21,15 +54,5 @@ class GameBoard private constructor(private val rows: Int = 6, private val colum
             println(error)
             return getGameBoard()
         }
-    }
-
-    fun print() {
-        val range = 1..columns
-        val (pipe, leftPipe, middlePipe, rightPipe) = listOf("║", "╚", "═╩", "═╝")
-
-        println("$rows X $columns board")
-        println(" " + (range).joinToString(" "))
-        board.forEach { println(it.joinToString(pipe, pipe, pipe)) }
-        println(leftPipe + middlePipe.repeat(columns - 1) + rightPipe)
     }
 }
