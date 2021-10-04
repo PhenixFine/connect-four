0fbb50eaae9bece1c544e4563caac9e82a7604b1
Danny <64429863+PhenixFine@users.noreply.github.com>
2021-10-04 19:29:58 -0400

    Stage 3 of 5 for JetBrains Academy - Kotlin - Connect Four project.
    ConnectFour.kt - changes
    • companion object - was moved to bottom after reading that it should be placed there. Updated getGame string value name from message to playerName to be more concise.
    • print() - was removed, and its content was moved into init
    • play() - rotates between players sending their moves to gameBoard's insertDisc() that returns either true or false depending on if they entered a valid column that isn't already full ( if true the next player is asked for their turn, and if not the same player is asked again ).

--- src/main/kotlin/ConnectFour.kt
+++ src/main/kotlin/ConnectFour.kt
@@ -4,18 +4,34 @@ class ConnectFour private constructor(
     private val player1: String, private val player2: String, private val gameBoard: GameBoard
 ) {
 
+    init {
+        println("$player1 VS $player2")
+        gameBoard.print()
+    }
+
+    fun play() {
+        var isPlayer1 = true
+        val turnMessage = { (if (isPlayer1) player1 else player2) + "'s turn:" }
+        val getDisc = { if (isPlayer1) 'o' else '*' }
+        var command = ""
+        val getCommand = { command = getString(turnMessage()); command }
+
+        while (getCommand() != "end") {
+            if (gameBoard.insertDisc(getDisc(), command)) {
+                gameBoard.print(false)
+                isPlayer1 = !isPlayer1
+            }
+        }
+        println("Game over!")
+    }
+
     companion object {
         fun getGame(): ConnectFour {
-            val message = "player's name:"
-            val player1 = getString("Connect Four\nFirst $message")
-            val player2 = getString("Second $message")
+            val playerName = "player's name:"
+            val player1 = getString("Connect Four\nFirst $playerName")
+            val player2 = getString("Second $playerName")
 
             return ConnectFour(player1, player2, GameBoard.getGameBoard())
         }
-    }
-
-    fun print() {
-        println("$player1 VS $player2")
-        gameBoard.print()
     }
 }
