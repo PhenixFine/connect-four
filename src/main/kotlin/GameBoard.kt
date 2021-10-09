ce6d6548a5fea03f182f5fd579f0a8acb0ea7154
Danny <64429863+PhenixFine@users.noreply.github.com>
2021-10-09 19:31:18 -0400

    Stage 5 of 5 for JetBrains Academy - Kotlin - Connect Four project.
    GameBoard.kt - changes:
    • values board and boardMap were changed to variables. Board is initialized with a call to getBoard() and boardMap with getBoardMap(), both of which contain what the two variables were originally initialized to.
    • print() - the option for print column rows info was removed, so that the line could be called separately.
    • printColumnRows() - prints the rows X columns header.
    • insertDisc() - update for rename of checkWin() to isWin()
    • isWin() - was originally checkWin() but was renamed to be more precise. The inner lambda that was named isWin() was moved to a function and renamed checkWin().
    • checkPath() - variable discCount was renamed to just count, as the name discCount seemed kind of confusing. The lambdas isValid() and isDisc() were turned into functions and adjusted to have the variables passed to them.
    • resetGame() - used the reset the game board. Will reset the necessary variables when called.
    • companion object - lambda notRange was moved to a function.

--- src/main/kotlin/GameBoard.kt
+++ src/main/kotlin/GameBoard.kt
@@ -2,21 +2,22 @@ import util.getString
 import Direction.*
 
 class GameBoard private constructor(private val rows: Int = 6, private val columns: Int = 7) {
-    private val board = Array(rows) { Array(columns) { ' ' } }
+    private var board = getBoard()
     private val columnRange = 1..columns
     private val rowRange = 1..rows
-    private val boardMap = columnRange.associateWith { rows }.toMutableMap()
+    private var boardMap = getBoardMap()
     var gameWon = false
         private set
 
-    fun print(printColumnRows: Boolean = true) {
+    fun print() {
         val (pipe, leftPipe, middlePipe, rightPipe) = listOf("║", "╚", "═╩", "═╝")
 
-        if (printColumnRows) println("$rows X $columns board")
         println(" " + columnRange.joinToString(" "))
         board.forEach { println(it.joinToString(pipe, pipe, pipe)) }
         println(leftPipe + middlePipe.repeat(columns - 1) + rightPipe)
     }
+
+    fun printColumnRows() = println("$rows X $columns board")
 
     fun insertDisc(disc: Char, columnString: String): Boolean {
         val column = columnString.toIntOrNull()
@@ -27,7 +28,7 @@ class GameBoard private constructor(private val rows: Int = 6, private val colum
             boardMap[column]?.let { row: Int ->
                 if (row != 0) {
                     updateBoard(disc, row, column)
-                    gameWon = checkWin(disc, row, column)
+                    gameWon = isWin(disc, row, column)
                     return true
                 } else error = "Column $column is full"
             }
@@ -41,28 +42,39 @@ class GameBoard private constructor(private val rows: Int = 6, private val colum
         boardMap[column] = row - 1
     }
 
-    private fun checkWin(disc: Char, row: Int, column: Int): Boolean {
-        val isWin = { path1: Direction, path2: Direction ->
-            checkPath(disc, row, column, path1) + checkPath(disc, row, column, path2) >= 3
-        }
+    private fun isWin(disc: Char, row: Int, column: Int) = checkWin(disc, row, column, LEFT, RIGHT)
+            || checkWin(disc, row, column, UP, DOWN) || checkWin(disc, row, column, UP_LEFT, DOWN_RIGHT)
+            || checkWin(disc, row, column, DOWN_LEFT, UP_RIGHT)
 
-        return isWin(LEFT, RIGHT) || isWin(UP, DOWN) || isWin(UP_LEFT, DOWN_RIGHT) || isWin(DOWN_LEFT, UP_RIGHT)
-    }
+    private fun checkWin(disc: Char, row: Int, column: Int, path1: Direction, path2: Direction) =
+        checkPath(disc, row, column, path1) + checkPath(disc, row, column, path2) >= 3
 
     private fun checkPath(disc: Char, row: Int, column: Int, path: Direction): Int {
-        var discCount = 0
+        var count = 0
         var shift = path.shift(row, column)
-        val isValid = { rowRange.contains(shift.first) && columnRange.contains(shift.second) }
-        val isDisc = { board[shift.first - 1][shift.second - 1] == disc }
 
-        while (isValid() && isDisc()) {
-            discCount++
+        while (isValid(shift) && isDisc(shift, disc)) {
+            count++
             shift = path.shift(shift.first, shift.second)
         }
-        return discCount
+        return count
     }
 
+    private fun isValid(shift: Pair<Int, Int>) = rowRange.contains(shift.first) && columnRange.contains(shift.second)
+
+    private fun isDisc(shift: Pair<Int, Int>, disc: Char) = board[shift.first - 1][shift.second - 1] == disc
+
     fun gameOver() = gameWon || boardMap.all { it.value == 0 }
+
+    fun resetGame() {
+        if (gameWon) gameWon = false
+        board = getBoard()
+        boardMap = getBoardMap()
+    }
+
+    private fun getBoard() = Array(rows) { Array(columns) { ' ' } }
+
+    private fun getBoardMap() = columnRange.associateWith { rows }.toMutableMap()
 
     companion object {
         fun getGameBoard(): GameBoard {
@@ -73,7 +85,6 @@ class GameBoard private constructor(private val rows: Int = 6, private val colum
 
             if (dimensions.isEmpty()) return GameBoard() else if (dimensions.matches(regex)) {
                 val (num1, num2) = dimensions.split("x", "X").map { it.toInt() }
-                val notRange = { num: Int -> !(5..9).contains(num) }
                 val wrongDimension = if (notRange(num1)) "rows" else if (notRange(num2)) "columns" else ""
                 if (wrongDimension.isEmpty()) return GameBoard(num1, num2) else {
                     error = "Board $wrongDimension should be from 5 to 9"
@@ -82,5 +93,7 @@ class GameBoard private constructor(private val rows: Int = 6, private val colum
             println(error)
             return getGameBoard()
         }
+
+        private fun notRange(num: Int) = !(5..9).contains(num)
     }
 }
