33539b00e8fe4677657488b0d7d456334a73f594
Danny <64429863+PhenixFine@users.noreply.github.com>
2021-10-09 20:23:20 -0400

    Stage 5 of 5 for JetBrains Academy - Kotlin - Connect Four project.
    For ConnectFour.kt and GameBoard.kt I replaced named lambdas with functions ( I found out using lambdas as functions adds overhead, so I'm going to try and limit their use ).
    
    ConnectFour.kt - changes:
    • added Int totalGames to the constructor
    • isMultiple - Boolean that informs if there will be more than one game
    • score - variable of Int pairs to keep track of each player's score ( if it isMultiple is true ).
    • isPlayer1 and command - moved from play()
    • currentPlayer - used to get the current player using a getter that returns a player based on the value of isPlayer1
    • init was adjusted to no longer print the gameBoard, and instead call printColumnRows() of gameBoard; and then print an appropriate message depending on if it's a single game or multiple game.
    play() - with the exception of the last line, the contents were moved to playGame(). It now just makes a call to playGame() and then prints the game over message.
    playGame() - it repeats the actions of what was in play() the number of times the players want to play the game. Lambda values turnMessage, getDisc, and getCommand were removed. Before the while loop the head for a multi game is printed with game number ( if isMultiple is true ); and then the gameBoard is printed. The call to getCommand within the while loop check was replaced with isContinue(). The call to insertDisc() of the gameBoard was adjusted to what was the contents of the getDisc lambda. The setting of drawOrWin for a win condition was adjusted to make use of the currentPlayer value. Once the while loop is complete it checks to confirm that the gameBoard gameOver() is true, if not it'll return to the calling function ( which would mean the user typed "end" ). A call to multipleGame() was added at the end ( which will only get called for a multi game ).
    • isContinue() - adjusted version of getCommand, where instead of returning the command, it returns true or false depending on if the user typed "end"
    • multipleGame() - sets the scores for each user dependent on if the current user won or if it was a draw. The score is then printed, and a call to resetGame() of gameBoard is made.
    • companion object - the return is adjusted to return the value for totalGames using getGameTotal(). getGameTotal() gets the total games from the user, returning 1 if the user pressed enter, or the number for any number that is greater than zero. If the user types in an invalid number it prints an error and then returns itself to attempt to get the total games from the user again.

--- src/main/kotlin/ConnectFour.kt
+++ src/main/kotlin/ConnectFour.kt
@@ -1,34 +1,61 @@
 import util.getString
 
 class ConnectFour private constructor(
-    private val player1: String, private val player2: String, private val gameBoard: GameBoard
+    private val player1: String,
+    private val player2: String,
+    private val gameBoard: GameBoard,
+    private val totalGames: Int
 ) {
+
+    private val isMultiple = totalGames > 1
+    private var score = Pair(0, 0)
+    private var isPlayer1 = true
+    private val currentPlayer get() = if (isPlayer1) player1 else player2
+    private var command = ""
 
     init {
         println("$player1 VS $player2")
-        gameBoard.print()
+        gameBoard.printColumnRows()
+        println(if (isMultiple) "Total $totalGames games" else "Single game")
     }
 
     fun play() {
-        var isPlayer1 = true
-        val turnMessage = { (if (isPlayer1) player1 else player2) + "'s turn:" }
-        val getDisc = { if (isPlayer1) 'o' else '*' }
-        var drawOrWin = "It is a draw"
-        var command = ""
-        val getCommand = { command = getString(turnMessage()); command }
+        playGame()
+        println("Game over!")
+    }
 
-        while (!gameBoard.gameOver() && getCommand() != "end") {
-            if (gameBoard.insertDisc(getDisc(), command)) {
-                gameBoard.print(false)
-                if (gameBoard.gameWon) {
-                    drawOrWin = "Player " + (if (isPlayer1) player1 else player2) + " won"
-                } else {
-                    isPlayer1 = !isPlayer1
+    private fun playGame() {
+        repeat(totalGames) { gameCount ->
+            var drawOrWin = "It is a draw"
+
+            if (gameCount > 0) isPlayer1 = (gameCount) % 2 == 0
+            if (isMultiple) println("Game #${gameCount + 1}")
+            gameBoard.print()
+            while (!gameBoard.gameOver() && isContinue()) {
+                if (gameBoard.insertDisc(if (isPlayer1) 'o' else '*', command)) {
+                    gameBoard.print()
+                    if (gameBoard.gameWon) {
+                        drawOrWin = "Player $currentPlayer won"
+                    } else isPlayer1 = !isPlayer1
                 }
             }
+            if (!gameBoard.gameOver()) return
+            println(drawOrWin)
+            if (isMultiple) multipleGame()
         }
-        if (gameBoard.gameOver()) println(drawOrWin)
-        println("Game over!")
+    }
+
+    private fun isContinue(): Boolean {
+        command = getString("$currentPlayer's turn:")
+        return command != "end"
+    }
+
+    private fun multipleGame() {
+        score = if (gameBoard.gameWon) {
+            if (isPlayer1) Pair(score.first + 2, score.second) else Pair(score.first, score.second + 2)
+        } else Pair(score.first + 1, score.second + 1)
+        println("Score\n$player1: ${score.first} $player2: ${score.second}")
+        gameBoard.resetGame()
     }
 
     companion object {
@@ -37,7 +64,21 @@ class ConnectFour private constructor(
             val player1 = getString("Connect Four\nFirst $playerName")
             val player2 = getString("Second $playerName")
 
-            return ConnectFour(player1, player2, GameBoard.getGameBoard())
+            return ConnectFour(player1, player2, GameBoard.getGameBoard(), getGameTotal())
+        }
+
+        private fun getGameTotal(): Int {
+            val message = """
+                Do you want to play single or multiple games?
+                For a single game, input 1 or press Enter
+                Input a number of games:
+                """.trimIndent()
+            val totalString = getString(message)
+            val total = if (totalString.isEmpty()) 1 else totalString.toIntOrNull()
+
+            if (total != null && total > 0) return total
+            println("Invalid input")
+            return getGameTotal()
         }
     }
 }
